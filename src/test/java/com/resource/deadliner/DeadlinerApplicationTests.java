189aacb38f4b4e18a3463b5a7804f7322f0eb321
ChiefGnomick <chiefgnomick@gmail.com>
2025-02-20 18:31:47 +0400

    init

---
+++ src/test/java/com/resource/deadliner/DeadlinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resource.deadliner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadlinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
